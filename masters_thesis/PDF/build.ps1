545591b8d6ff7becb5208a1ee7f41a90a2ef2e4b
Bruno Henrique Nunes <bnunes@bi4all.pt>
2026-05-06 03:13:01 -0300

    feat: Refactor thesis build process and update .gitignore for PDF artifacts

---
+++ masters_thesis/PDF/build.ps1
@@ -0,0 +1,57 @@
+$ErrorActionPreference = "Stop"
+
+$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$SourceDir = Resolve-Path (Join-Path $ScriptDir "..\New_Thesis")
+
+$ArtifactPatterns = @(
+    "*.aux",
+    "*.bbl",
+    "*.bcf",
+    "*.blg",
+    "*.fls",
+    "*.fdb_latexmk",
+    "*.ilg",
+    "*.lof",
+    "*.log",
+    "*.lot",
+    "*.nlo",
+    "*.nls",
+    "*.out",
+    "*.pdf",
+    "*.run.xml",
+    "*.synctex.gz",
+    "*.toc"
+)
+
+function Move-BuildArtifacts {
+    foreach ($pattern in $ArtifactPatterns) {
+        Get-ChildItem -LiteralPath $SourceDir -File -Filter $pattern -Force -ErrorAction SilentlyContinue |
+            ForEach-Object {
+                Move-Item -LiteralPath $_.FullName -Destination (Join-Path $ScriptDir $_.Name) -Force
+            }
+    }
+}
+
+Push-Location $SourceDir
+try {
+    xelatex -interaction=nonstopmode -halt-on-error tese.tex
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+    biber tese
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+    if (Test-Path "tese.nlo") {
+        makeindex tese.nlo -s nomencl.ist -o tese.nls
+        if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+    }
+
+    xelatex -interaction=nonstopmode -halt-on-error tese.tex
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+    xelatex -interaction=nonstopmode -halt-on-error tese.tex
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+}
+finally {
+    Pop-Location
+    Move-BuildArtifacts
+}
