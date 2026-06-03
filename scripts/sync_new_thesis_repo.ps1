8199103a43ebe3c05da29a94eec93f47ea8252a5
Bruno Henrique Nunes <bnunes@bi4all.pt>
2026-06-03 09:42:28 -0300

    Update RemoteUrl parameter to point to the new thesis repository version

--- scripts/sync_new_thesis_repo.ps1
+++ scripts/sync_new_thesis_repo.ps1
@@ -62,7 +62,7 @@ param(
 
     [string]$Prefix = "masters_thesis/New_Thesis",
 
-    [string]$RemoteUrl = "https://github.com/bhnunes/thesis_overleaf.git",
+    [string]$RemoteUrl = "https://github.com/bhnunes/thesis_overleaf_v2.git",
 
     [switch]$NoSquash,
 
